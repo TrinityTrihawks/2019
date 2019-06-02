f208547f4cfb260200e239d71fa55201bb9610d3
Charles Nykamp <cqnykamp@gmail.com>
2019-06-02 12:52:38 -0500

    Use Travis CI

--- src/test/java/frc/robot/commands/TeleopDriveTest.java
+++ src/test/java/frc/robot/commands/TeleopDriveTest.java
@@ -2,17 +2,14 @@ package frc.robot.commands;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyDouble;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import org.junit.*;
 import org.mockito.*;
-import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.subsystems.*;
 import frc.robot.GlobalState;
 import frc.robot.OI;
-import frc.robot.commands.*;
 
 public class TeleopDriveTest {
 
@@ -37,12 +34,5 @@ public class TeleopDriveTest {
     public void shouldRequireDrivetrain() {
         assertTrue(teleopDrive.doesRequire(drivetrainMock));
     }
-
-    // @Test
-    // public void itShouldRunDriveWhenScheduled() {
-    //     Scheduler.getInstance().add(teleopDrive);
-    //     Scheduler.getInstance().run();
-    //     verify(drivetrainMock).Drive(anyDouble(), anyDouble());
-    // }
 
 }
